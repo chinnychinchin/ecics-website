9ee7fa519652f09a48c42968675d3f02cada5046
chinnychinchin <chinyan77@gmail.com>
2021-01-19 21:24:12 +0800

    done up a simple landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule, Router } from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MainComponent } from './components/main.component';
+import { AboutComponent } from './components/about.component';
+
+const ROUTES: Routes = [
+
+{path: '', component: MainComponent},
+{path: 'about', component: AboutComponent}
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
